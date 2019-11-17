42453cab95d80fdf5d21d9f50a7c0822018f658e
Chris Jagielski <chrisjagielski@gmail.com>
2019-11-17 16:16:06 -0500

    overview comments

---
+++ test.py
@@ -0,0 +1,11 @@
+# Superdog
+
+"""
+ Let people create a "league" (group) of up to 10 players
+ Create team names
+ Get the available games for the week
+ Select your team to beat the spread
+ Track who's right or not
+ Track if anyone's superdog wins outright
+ Log outright wins and overall W/L records of all teams
+"""
